70d531d58f44a5a0a6c00eaa144aa53d6fef5692
automation <inhae.kim@falconstor.com>
2013-06-13 14:31:55 -0400

    adding scrap

---
+++ scrap/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '72b245fbb83cc74108a7d75090b3e4e7a30dc3117685f0f06195e58f7a68dc525abdffb534a9b1b97d6bad37d47666e73260cdcf06b69d1c20edc6c2add5ae0c'
